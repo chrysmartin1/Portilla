c587263783af221e537b573c103b2517f96c8955
chrys <martinchrys1@gmail.com>
2025-06-05 15:59:50 +0100

    first commit

---
+++ camp/spotify/urls.py
@@ -0,0 +1,9 @@
+
+from .import views
+from django.urls import path,include
+
+urlpatterns = [
+    path('',views.index,name='index'),
+    path('john/',views.trial,name='trial'),
+    path('about/',views.about,name='about')
+]

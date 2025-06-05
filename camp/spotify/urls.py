b4ebc3233ae52de48b4877201c19b7b233360d52
unknown <nwosuemmanuel159@gmail.com>
2025-06-05 17:38:31 +0100

    corrected the base.html and the index.html

--- camp/spotify/urls.py
+++ camp/spotify/urls.py
@@ -4,6 +4,6 @@ from django.urls import path,include
 
 urlpatterns = [
     path('',views.index,name='index'),
-    path('john/',views.trial,name='trial'),
+    # path('john/',views.trial,name='trial'),
     path('about/',views.about,name='about')
 ]
--- camp/spotify/views.py
+++ camp/spotify/views.py
@@ -2,9 +2,9 @@ from django.shortcuts import render
 from django.http import HttpResponse
 # Create your views here.
 
-def trial(request):
-    my_dict = {'insert_me':" Much Joy"}
-    return render(request,'camp2/trial.html',context=my_dict) #this context is linking what he hav in my_dict
+# def trial(request):
+#     my_dict = {'insert_me':" Much Joy"}
+#     return render(request,'camp2/trial.html',context=my_dict) #this context is linking what he hav in my_dict
 def index(request):
     return render(request,'camp2/index.html')
 def about(request):
